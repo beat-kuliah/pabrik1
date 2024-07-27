ef37019898708a9217c2bfbd2b2752f6463d047d
Fransiskus <fransiskusfernando095@gmail.com>
2024-07-27 20:15:45 +0700

    fix https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -15,13 +16,16 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -36,7 +40,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -44,74 +50,83 @@
                     <!-- Left Side Of Navbar -->
                     @guest
                     @else
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/user">User</a>
-                        </li>
-                        @if (Auth::user()->role[0] == 'ADMIN' || Auth::user()->role[0] == 'STAFF_GUDANG' || Auth::user()->role[0] == 'ADMIN_GUDANG')
-                        <li class="nav-item">
-                            <a class="nav-link" href="/vendor">Vendor</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/gudang">Gudang</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/barang">Barang</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/penjualan">Penjualan</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/retur">Retur</a>
-                        </li>
-                        @endif
-                        @if (Auth::user()->role[0] == 'ADMIN' || Auth::user()->role[0] == 'MANAGER' || Auth::user()->role[0] == 'KEUANGAN' || Auth::user()->role[0] == 'PURCHASING')
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Report
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/report-stok">Stok</a></li>
-                                <li><a class="dropdown-item" href="/report-account">Penjualan</a></li>
-                            </ul>
-                        </li>
-                        @endif
-                    </ul>
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/user">User</a>
+                            </li>
+                            @if (Auth::user()->role[0] == 'ADMIN' ||
+                                    Auth::user()->role[0] == 'STAFF_GUDANG' ||
+                                    Auth::user()->role[0] == 'ADMIN_GUDANG')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/vendor">Vendor</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/gudang">Gudang</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/barang">Barang</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/penjualan">Penjualan</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/retur">Retur</a>
+                                </li>
+                            @endif
+                            @if (Auth::user()->role[0] == 'ADMIN' ||
+                                    Auth::user()->role[0] == 'MANAGER' ||
+                                    Auth::user()->role[0] == 'KEUANGAN' ||
+                                    Auth::user()->role[0] == 'PURCHASING')
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Report
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="/report-stok">Stok</a></li>
+                                        <li><a class="dropdown-item" href="/report-account">Penjualan</a></li>
+                                    </ul>
+                                </li>
+                            @endif
+                        </ul>
                     @endguest
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @endif
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->username }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-
-                                <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#updatePassword">Edit Password</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->username }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                                    <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#updatePassword">Edit
+                                        Password</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
